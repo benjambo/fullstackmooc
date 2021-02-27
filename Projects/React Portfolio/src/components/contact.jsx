788f5617d86e76b6978508c5e066a8e366758973
Benjamin Schelling <beni.schelling@gmail.com>
2021-02-28 00:16:27 +0200

    First Commit

--- Projects/React Portfolio/src/components/contact.jsx
+++ Projects/React Portfolio/src/components/contact.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-//import imageOverlay from "../img/blur.jpg";
+import imageOverlay from "../img/blur.jpg";
 
 class Contact extends React.Component {
   render() {
